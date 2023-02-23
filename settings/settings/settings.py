cb05853af8482b80a85af4ffc5f9eeff2f034b67
Kirill Chernogrivov <kirill.2201@inbox.ru>
2023-02-23 20:11:24 +0700

    working with docker

--- settings/settings/settings.py
+++ settings/settings/settings.py
@@ -59,7 +59,7 @@ DATABASES = {
         'NAME': "postgres",
         'USER': "postgres",
         'PASSWORD': "postgres",
-        'HOST': "localhost",
+        'HOST': "postgres",
         'PORT': "5432",
     }
 }
