405c2df974ee6d3535cb4f9907f17bc2d9fd707b
FloppaSlave <korstim18@gmail.com>
2023-03-02 19:31:10 +0700

    change DB

--- settings/settings/settings.py
+++ settings/settings/settings.py
@@ -54,30 +54,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'settings.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "postgres",
-#         'USER': "postgres",
-#         'PASSWORD': "postgres",
-#         'HOST': "postgres",
-#         'PORT': "5432",
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "postgres",
+        'HOST': "postgres",
+        'PORT': "5432",
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
