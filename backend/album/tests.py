4fb4cc983928c953667943f534afead68b6eee48
LedxDeliveryFlopp <korstim18@gmaiol.com>
2023-06-21 11:12:51 +0700

    add unit test

--- backend/album/tests.py
+++ backend/album/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from user.models import Profile
 
-# Create your tests here.
+
+class AlbumTest(TestCase):
+
+    def setUp(self):
+        self.profile = Profile.objects.create(username='test', password='test', type_user='Исполнитель')
+        self.profile.save()
+        self.client.login(username='test', password='test')
+
+    def test_add_album(self):
+        response = self.client.post('/api/create-album/', data={
+            'title': 'test',
+            'author': 1,
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_all_music(self):
+        response = self.client.get('/api/all-album/')
+        response.json()
+        self.assertEqual(response.status_code, 200)
--- backend/music/tests.py
+++ backend/music/tests.py
@@ -1,3 +1,27 @@
+from user.models import Profile
+from .models import Music, Genre
 from django.test import TestCase
 
-# Create your tests here.
+
+class MusicTest(TestCase):
+
+    def setUp(self):
+        self.profile = Profile.objects.create(username='test', password='test', type_user='Исполнитель')
+        self.profile.save()
+        self.client.login(username='test', password='test')
+
+        self.genre = Genre.objects.create(title='test')
+        self.genre.save()
+
+    def test_add_music(self):
+        response = self.client.post('/api/create-music/', data={
+            'title': 'test',
+            'author': 1,
+            'genre': 1
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_all_music(self):
+        response = self.client.get('/api/all-music/')
+        response.json()
+        self.assertEqual(response.status_code, 200)
